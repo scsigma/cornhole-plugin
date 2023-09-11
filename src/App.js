83beb7af1697c15ca90a3f4442ba7f17028c6f2e
scsigma <sully@sigmacomputing.com>
2023-09-11 16:38:48 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [bagCoordinates, setBagCoordinates] = useState([]);
+  
+  const handleBoardClick = (event) => {
+    const { clientX, clientY } = event;
+    console.log("clientX", clientX);
+    console.log("clientY", clientY)
+  
+    // Get the parent div element (the board in this case).
+    const board = document.querySelector('.board');
+  
+    // Get the position of the board relative to the viewport.
+    const boardRect = board.getBoundingClientRect();
+    console.log("boardRect", boardRect);
+  
+    // Calculate coordinates relative to the board.
+    const xRelativeToBoard = clientX - boardRect.left;
+    const yRelativeToBoard = clientY - boardRect.top;
+  
+    // Now, xRelativeToBoard and yRelativeToBoard are relative to the board.
+    console.log(`Relative to board - X: ${xRelativeToBoard}, Y: ${yRelativeToBoard}`);
+
+    // Update the state with the new bag placement.
+    // setBagCoordinates([...bagCoordinates, [xRelativeToBoard,yRelativeToBoard]]);
+    setBagCoordinates([...bagCoordinates, {'x for data': xRelativeToBoard,'y for data': yRelativeToBoard, 'x for css': clientX, 'y for css': clientY}]);
+
+    console.log("Bag coordinates", bagCoordinates);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="cornhole-game" style={{ border: "blue 2px solid", width: "600px", height: "300px"}}>
+      <div className="board" onClick={handleBoardClick} style={{ border: "red 2px solid", width: "200px", height: "75px" , marginLeft: "100px", marginTop: "100px"}}>
+        {/* Render bags at bagCoordinates */}
+        {/* {bagCoordinates.map((coordinates, index) => (
+          // <div key={index} className="bag" style={{ left: coordinates.x, top: coordinates.y, width: "10px", height: "10px", backgroundColor:"red", position: "fixed" }}></div>
+          <div key={index} className="bag" style={{ left: coordinates['x for css'] - 5, top: coordinates['y for css'] - 5, width: "10px", height: "10px", backgroundColor:"red", position: "fixed" }}></div>
+        ))} */}
+      </div>
+      {bagCoordinates.map((coordinates, index) => (
+          // <div key={index} className="bag" style={{ left: coordinates.x, top: coordinates.y, width: "10px", height: "10px", backgroundColor:"red", position: "fixed" }}></div>
+          <div key={index} className="bag" style={{ left: coordinates['x for css'] - 5, top: coordinates['y for css'] - 5, width: "10px", height: "10px", backgroundColor:"red", position: "fixed" }}></div>
+        ))}
     </div>
   );
 }
